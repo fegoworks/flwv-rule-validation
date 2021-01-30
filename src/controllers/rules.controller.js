079b62b2e8934e0dc9665be98be1c1e438eb29db
unknown <fegoworks@hotmail.com>
2021-01-31 00:14:38 +0100

    feat - build out controller logic

---
+++ src/controllers/rules.controller.js
@@ -0,0 +1,35 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable class-methods-use-this */
+import { BadRequest } from '../helpers/responseHandler';
+import RulesServices from '../services/rules.service';
+
+class RulesController {
+  static async validateRule(req, res, next) {
+    const { body: { rule, data } } = req;
+    const checker = new RulesServices(res, rule, data);
+    // console.log(checker);
+    try {
+      // check if body data is an array
+      if (Array.isArray(data) || typeof data === 'string') {
+        checker.checkDataFields();
+        checker.checkConditions();
+        return;
+      }
+      // check if body data is an object
+      if (typeof data === 'object' && !Array.isArray(data)) {
+        checker.checkObjectDataFields();
+        if (checker.isNested()) {
+          checker.checkNestedFieldConditions();
+        }
+        checker.checkConditions();
+        return;
+      }
+
+      throw new BadRequest('data should be an object, string or an array.', null);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default RulesController;
