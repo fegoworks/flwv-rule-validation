bc4524ea3225891667e073dcce7969dfbaca4057
unknown <fegoworks@hotmail.com>
2021-01-30 21:40:57 +0100

    feat - set up initial rules route

---
+++ src/routes/rules.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+// const router = express.Router();
+
+// router.post('/auth/create-user',
+//   validate.validateBody(validate.schemas.authSchema),
+//   UserController.createUser);
+
+// export default router;
